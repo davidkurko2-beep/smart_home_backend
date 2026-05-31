8559bd886b9e7d8747b4ecca2dd3f075bbe025ac
davidkurko2-beep <davidkurko2@gmail.com>
2026-05-31 11:55:17 +0300

    backend

--- routers/houses.py
+++ routers/houses.py
@@ -17,8 +17,9 @@ def create_house(house:HouseCreate,db:Session=Depends(get_db)):
     user=db.query(User).filter(User.id==house.user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-new_house=House(
-    name = house.name,
-    address=house.address,
-    user_id=house.user_id
-)
+    new_house=House(
+        name = house.name,
+        address=house.address,
+        user_id=house.user_id)
+    db.add(new_house)
+    db.commit()
