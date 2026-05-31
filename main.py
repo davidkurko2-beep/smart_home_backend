4761f0e55ba5b02fa3912eaadbf584243da6a791
davidkurko2-beep <davidkurko2@gmail.com>
2026-05-31 11:52:12 +0300

    backend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from routers.houses import router as house_router
 from database import Base, engine
 from routers.users import router as users_router
 app=FastAPI()
@@ -7,6 +7,7 @@ app=FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(users_router)
+app.include_router(house_router)
 @app.get("/")
 def home():
     return {"message": "API works"}
