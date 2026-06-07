6074efc8dfcd6db97a2d67353369ca7c3f89f466
davidkurko2-beep <davidkurko2@gmail.com>
2026-06-07 11:48:39 +0300

    backend

--- routers/devices.py
+++ routers/devices.py
@@ -4,10 +4,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
+from models import device
 from models.device import Device
 from models.house import House
 from models.room import Room
-from schemas.device import DeviceCreate
+from schemas.device import DeviceCreate, DeviceUpdate
 
 router = APIRouter(prefix="/devices", tags=["Devices"])
 @router.post("/")
@@ -26,3 +27,35 @@ def create_device(device:DeviceCreate, db:Session=Depends(get_db)):
     db.commit()
     db.refresh(new_device)
     return new_device
+
+@router.get("/")
+def get_devices(db:Session=Depends(get_db)):
+    devices = db.query(House).all()
+    return devices
+@router.get("/{device_id}")
+def get_device(device_id: int, db:Session=Depends(get_db)):
+    device = db.query(Device).filter(Device.id==device_id).first()
+    if device is None:
+        raise HTTPException(status_code=404, detail="House not found")
+    return device
+
+@router.put("/{device_id}")
+def update_device(room_id: int, device_data: DeviceUpdate, db:Session=Depends(get_db)):
+    device = db.query(Room).filter(Room.id == room_id).first()
+    if device is None:
+        raise HTTPException(status_code=404, detail = "House not found")
+    device.name = device_data.name
+    device.type= device_data.type
+    device.status = device_data.status
+    device.is_online = device_data.is_online
+    db.commit()
+    db.refresh(device)
+    return device
+@router.delete("/device_id}")
+def delete_device(device_id:int, db:Session=Depends(get_db)):
+    device=db.query(Device).filter(Device.id == device_id).first()
+    if device is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(device)
+    db.commit()
+    return {"message": "Device Deleted"}
