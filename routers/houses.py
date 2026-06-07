7d3e8d1734cd04038a688649811c2b4ec88e0f87
davidkurko2-beep <davidkurko2@gmail.com>
2026-06-07 09:39:40 +0300

    backend

--- routers/houses.py
+++ routers/houses.py
@@ -9,7 +9,7 @@ from database import get_db
 from models import house
 from models.house import House
 from models.user import User
-from schemas.house import HouseCreate
+from schemas.house import HouseCreate, HouseUpdate
 
 router = APIRouter(prefix="/houses", tags=["Houses"])
 @router.post("/")
@@ -23,3 +23,27 @@ def create_house(house:HouseCreate,db:Session=Depends(get_db)):
         user_id=house.user_id)
     db.add(new_house)
     db.commit()
+    db.refresh(new_house)
+    return new_house
+
+@router.get("/")
+def get_houses(db:Session=Depends(get_db)):
+    houses = db.query(House).all()
+    return houses
+@router.get("/{house_id}")
+def get_house(house_id: int, db:Session=Depends(get_db)):
+    house = db.query(House).filter(House.id==house_id).first()
+    if house is None:
+        raise HTTPException(status_code=404, detail="House not found")
+    return house
+
+@router.put("/{house_id}")
+def update_house(house_id: int, house: HouseUpdate, db:Session=Depends(get_db)):
+    house = db.query(House).filter(House.id == house_id).first()
+    if house is None:
+        raise HTTPException(status_code=404, detail = "House not found")
+    house.name = house_data.name
+    house.address = house_data.addres
+    db.commit()
+    db.refresh(house)
+    return house
