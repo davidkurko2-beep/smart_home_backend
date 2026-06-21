451acf7a324001c4db46d6281d83467c27534cc4
davidkurko2-beep <davidkurko2@gmail.com>
2026-06-21 10:04:04 +0300

    smart home 90% accuracy

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from auth import verify_password, create_access_token
@@ -9,12 +10,12 @@ from schemas.auth import LoginData
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 @router.post("/login")
-def login(data:LoginData,db:Session=Depends(get_db)):
-    user=db.query(User).filter(User.email==data.email).first()
+def login(from_data:OAuth2PasswordRequestForm=Depends(),db:Session=Depends(get_db)):
+    user=db.query(User).filter(User.email==from_data.username).first()
     if user is None:
         raise HTTPException(status_code=404, detail="Email or password incorrect")
 
-    if not verify_password(data.password, user.password):
+    if not verify_password(from_data.password, user.password):
         raise HTTPException(status_code=404, detail="Email or password incorrect")
     token=create_access_token(data={"user_id":user.id})
     return {
