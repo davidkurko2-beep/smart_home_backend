a4e5d85a8251f6dec3d6fd643e7fa0442653d4d2
davidkurko2-beep <davidkurko2@gmail.com>
2026-06-21 11:12:09 +0300

    smart home 90% accuracy

--- routers/rooms.py
+++ routers/rooms.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
+from auth import get_current_user
 from database import get_db
 from models.house import House
 from models.room import Room
+from models.user import User
 from schemas.room import RoomCreate, RoomUpdate
 
 router=APIRouter(prefix="/rooms", tags=["Rooms"])
@@ -24,19 +26,19 @@ def create_room(room: RoomCreate,
     return new_room
 
 @router.get("/")
-def get_rooms(db:Session=Depends(get_db)):
-    rooms = db.query(Room).all()
+def get_rooms(db:Session=Depends(get_db),current_user:User=Depends(get_current_user)):
+    rooms = db.query(Room).filter(Room.user_id == current_user.id).all()
     return rooms
 @router.get("/{room_id}")
-def get_room(room_id: int, db:Session=Depends(get_db)):
-    room = db.query(Room).filter(Room.id == room_id).first()
+def get_room(room_id: int, db:Session=Depends(get_db),current_user:User=Depends(get_current_user)):
+    room = db.query(Room).filter(Room.id == room_id,Room.user_id==current_user.id).first()
     if room is None:
         raise HTTPException(status_code=404, detail="Room not found")
     return room
 
 @router.put("/{room_id}")
-def update_room(room_id: int, room_data: RoomUpdate,db:Session=Depends(get_db)):
-    room = db.query(Room).filter(Room.id == room_id).first()
+def update_room(room_id: int, room_data: RoomUpdate,db:Session=Depends(get_db),current_user:User=Depends(get_current_user)):
+    room = db.query(Room).filter(Room.id == room_id,Room.user_id==current_user.id).first()
     if room is None:
         raise HTTPException(status_code=404, detail="Room not found")
     room.name = room_data.name
@@ -44,8 +46,8 @@ def update_room(room_id: int, room_data: RoomUpdate,db:Session=Depends(get_db)):
     db.refresh(room)
     return room
 @router.delete("/{room_id}")
-def delete_room(room_id: int,db:Session=Depends(get_db)):
-    room = db.query(Room).filter(Room.id == room_id).first()
+def delete_room(room_id: int,db:Session=Depends(get_db),current_user:User=Depends(get_current_user)):
+    room = db.query(Room).filter(Room.id == room_id,Room.user_id==current_user.id).first()
     if room is None:
         raise HTTPException(status_code=404, detail="Room not found")
     db.delete(room)
